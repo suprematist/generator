de4093c12911d7afb31e1946662a843b9eb0a02a
Eduard Aksamitov <e@euaaaio.ru>
2025-05-21 18:58:58 +0300

    workflow(vite): add vue devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import vueDevtools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-	plugins: [vue()]
+	plugins: [
+		vue(),
+		vueDevtools(),
+	]
 })
